e9329d26628ff114b2fedf240e94efcd68c13f5d
lahirujay99 <lahirujayawardana94@gmail.com>
2023-03-19 17:02:41 +0530

    about us and terms and conditions completed

---
+++ app/src/main/java/com/example/mealmate/TermsAndConditions.kt
@@ -0,0 +1,11 @@
+package com.example.mealmate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TermsAndConditions : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_terms_and_conditions)
+    }
+}
