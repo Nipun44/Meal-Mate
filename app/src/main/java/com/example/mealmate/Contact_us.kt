c95e0de0a4ab324d48ab1df2fd6812d1af92e642
Umesh Dewasinghe <89865946+umeshdewasinghe@users.noreply.github.com>
2023-03-19 05:49:07 +0530

    contact us created

---
+++ app/src/main/java/com/example/mealmate/Contact_us.kt
@@ -0,0 +1,11 @@
+package com.example.mealmate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Contact_us : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_us)
+    }
+}
