42a6e7a8bdeafee6abb75a718d87fad7053f70c4
Umesh Dewasinghe <89865946+umeshdewasinghe@users.noreply.github.com>
2023-03-19 06:26:42 +0530

    profile started

---
+++ app/src/main/java/com/example/mealmate/Profile.kt
@@ -0,0 +1,11 @@
+package com.example.mealmate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
