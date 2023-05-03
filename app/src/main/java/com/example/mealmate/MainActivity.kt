061cca26af47b51769bd351b8d352b3eb895fa21
lahirujay99 <lahirujayawardana94@gmail.com>
2023-05-03 23:48:22 +0530

    fixed errors in manifest , add activity to register 2,3

--- app/src/main/java/com/example/mealmate/MainActivity.kt
+++ app/src/main/java/com/example/mealmate/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.mealmate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var notAccount : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        notAccount = findViewById(R.id.not_have_account_text)
+
+        notAccount.setOnClickListener {
+            val intent = Intent(this,Register::class.java)
+            startActivity(intent)
+        }
     }
 }
