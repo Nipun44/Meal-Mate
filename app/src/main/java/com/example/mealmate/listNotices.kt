45f19bb2b66b7a3a8068a87e476b945d53f34556
Nipun Ganepola <85781012+Nipun44@users.noreply.github.com>
2023-05-05 02:46:15 +0530

    create fetch activity

---
+++ app/src/main/java/com/example/mealmate/listNotices.kt
@@ -0,0 +1,11 @@
+package com.example.mealmate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class listNotices : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_notices)
+    }
+}
